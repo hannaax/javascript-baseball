01672d892252278857890abfdcfe2e5e676ea7ed
hannaax <hanan941008@gmail.com>
2023-10-05 19:27:21 +0900

    feat: 볼,스트라이크 개수를 반환하는 함수 및 낫싱 출력 구현

--- src/App.js
+++ src/App.js
@@ -6,34 +6,56 @@ const INPUT_MESSAGE = "숫자를 입력해주세요 : "
 class App {
   constructor() {
     this.userInput = ""
-    this.computerNumber = []
+    this.computerNumbers = []
   }
 
   printGameStartMessage() {
     MissionUtils.Console.print(GAME_START_MESSAGE)
   }
 
-  readUserInput() {
+  readUserInput(computerNumbers) {
     MissionUtils.Console.readLine(INPUT_MESSAGE, (userInput) => {
       this.userInput = userInput
       this.readUserInput()
+      let strike = this.countStrikeNumbers(computerNumbers, userInput)
+      let ball = this.countBallNumbers(computerNumbers, userInput)
+      console.log(strike, ball)
+      if (strike === 0 && ball === 0) {
+        MissionUtils.Console.print("낫싱")
+      }
     })
   }
 
-  generateRandomNumber() {
-    while (this.computerNumber.length < 3) {
+  countStrikeNumbers(computerNumbers, userInput) {
+    const count = computerNumbers.filter(
+      (number, index) => number == userInput[index]
+    ).length
+    return count
+  }
+
+  countBallNumbers(computerNumbers, userInput) {
+    const count = computerNumbers.filter(
+      (number, index) =>
+        userInput.includes(number) && number != userInput[index]
+    ).length
+    return count
+  }
+
+  generateRandomNumbers() {
+    while (this.computerNumbers.length < 3) {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9)
-      if (!this.computerNumber.includes(randomNumber)) {
-        this.computerNumber.push(randomNumber)
+      if (!this.computerNumbers.includes(randomNumber)) {
+        this.computerNumbers.push(randomNumber)
       }
     }
-    return computerNumber
+    console.log(this.computerNumbers)
+    return this.computerNumbers
   }
 
   play() {
+    let computerNumbers = this.generateRandomNumbers()
     this.printGameStartMessage()
-    this.generateRandomNumber()
-    this.readUserInput()
+    this.readUserInput(computerNumbers)
   }
 }
 
