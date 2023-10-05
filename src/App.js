0a441126cb558eb62a0aa2592ab8c513a58d3b06
hannaax <hanan941008@gmail.com>
2023-10-05 17:36:17 +0900

    feat: 컴퓨터가 랜덤 숫자 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,22 +6,34 @@ const INPUT_MESSAGE = "숫자를 입력해주세요 : "
 class App {
   constructor() {
     this.userInput = ""
-  }
-
-  readUserInput() {
-    MissionUtils.Console.readLine(INPUT_MESSAGE, (number) => {
-      this.userInput = number
-      this.getUserInput()
-    })
+    this.computerNumber = []
   }
 
   printGameStartMessage() {
     MissionUtils.Console.print(GAME_START_MESSAGE)
-    this.readUserInput()
+  }
+
+  readUserInput() {
+    MissionUtils.Console.readLine(INPUT_MESSAGE, (userInput) => {
+      this.userInput = userInput
+      this.readUserInput()
+    })
+  }
+
+  generateRandomNumber() {
+    while (this.computerNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9)
+      if (!this.computerNumber.includes(randomNumber)) {
+        this.computerNumber.push(randomNumber)
+      }
+    }
+    return computerNumber
   }
 
   play() {
     this.printGameStartMessage()
+    this.generateRandomNumber()
+    this.readUserInput()
   }
 }
 
